04120083d468e69ca021037bd7357affa36a77ef
“lastlySly <lastlySly@163.com>
2019-09-10 17:58:50 +0800

    百度人脸识别评估

--- src/main/java/com/lastlysly/utils/MyUtils.java
+++ src/main/java/com/lastlysly/utils/MyUtils.java
@@ -6,6 +6,9 @@ import com.google.common.collect.Maps;
 import com.lastlysly.view.UserInfo;
 import com.lastlysly.view.facedetails.DetectFaceDetailsInfoView;
 import com.lastlysly.view.facedetails.DetectFaceInfoView;
+import com.lastlysly.view.matchfaceresponse.CustomBzFileAndScoreView;
+import com.lastlysly.view.matchfaceresponse.CustomMatchFaceDataView;
+import com.lastlysly.view.matchfaceresponse.MatchFaceResponseView;
 import com.lastlysly.view.searchfaceresponse.SearchFaceResponseView;
 import com.lastlysly.view.searchfaceresponse.SearchFaceResultView;
 import com.lastlysly.view.searchfaceresponse.SearchFaceUserInfo;
@@ -63,7 +66,7 @@ public class MyUtils {
      * 扫描指定文件路径下所有人脸文件，并做人脸检测，并存入excel
      * @param filePath
      */
-    public static void scanFilesAndDetectFaceAndSaveExcel(String filePath) throws InterruptedException, IOException {
+    public static void scanFilesAndDetectFaceAndSaveExcel(String filePath,HSSFWorkbook hssfWorkbook) throws InterruptedException, IOException {
         List<String> filesPathList = Lists.newArrayList();
         MyFileUtils.getFilesPathList(filePath,filesPathList);
         List<DetectFaceInfoView> detectFaceInfoViewList = Lists.newArrayList();
@@ -71,17 +74,17 @@ public class MyUtils {
             File file = new File(filesPathList.get(i));
             DetectFaceInfoView res =MyFaceRecognitionUtils.detectFace(file,"5");
             res.setFilePath(filesPathList.get(i));
+            System.out.println("============scanFilesAndDetectFaceAndSaveExcel===============");
+            System.out.println(res);
             detectFaceInfoViewList.add(res);
             //            处理QPS 超限。当前为2。
             Thread.sleep(500);
         }
         //输出Excel文件
-        File excel = new File("C:\\Users\\lastlySly\\Desktop\\test\\test.xls");
-        try {
-            detectFaceInfoToExcel(detectFaceInfoViewList,excel);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        File excel = new File("C:\\Users\\lastlySly\\Desktop\\test\\test.xls");
+
+        detectFaceInfoToExcel(detectFaceInfoViewList,hssfWorkbook);
+
 
     }
 
@@ -89,12 +92,11 @@ public class MyUtils {
     /**
      * 人脸检测数据导出到excel
      * @param detectFaceInfoViewList
-     * @param outFile
      * @throws IOException
      */
-    public static void detectFaceInfoToExcel(List<DetectFaceInfoView> detectFaceInfoViewList,File outFile) throws IOException {
+    public static void detectFaceInfoToExcel(List<DetectFaceInfoView> detectFaceInfoViewList,HSSFWorkbook hssfWorkbook) throws IOException {
 
-        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+//        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
         HSSFSheet hssfSheet = hssfWorkbook.createSheet("人脸检测数据");
         hssfSheet.setDefaultRowHeightInPoints(100);
         hssfSheet.setDefaultColumnWidth(30);
@@ -129,7 +131,7 @@ public class MyUtils {
         int row = 2;
         excelSetDetectFaceData(detectFaceInfoViewList, hssfWorkbook, hssfSheet, cellStyle, patriarch,row);
 
-        hssfWorkbook.write(outFile);
+//        hssfWorkbook.write(outFile);
     }
 
     /**
@@ -227,7 +229,7 @@ public class MyUtils {
      * @throws IOException
      * @throws InterruptedException
      */
-    public static void scanFilesAndSearchFaceAndSaveExcel(String filePath) throws IOException, InterruptedException {
+    public static void scanFilesAndSearchFaceAndSaveExcel(String filePath,HSSFWorkbook hssfWorkbook) throws IOException, InterruptedException {
         List<String> filesPathList = Lists.newArrayList();
         MyFileUtils.getFilesPathList(filePath,filesPathList);
         List<SearchFaceResponseView> searchFaceResponseViewList = Lists.newArrayList();
@@ -236,14 +238,15 @@ public class MyUtils {
             SearchFaceResponseView searchFaceResponseView =
                     MyFaceRecognitionUtils.searchFace(file,"test",null,"1");
             searchFaceResponseView.setFile(file);
+            System.out.println("==============scanFilesAndSearchFaceAndSaveExcel===============");
             System.out.println(searchFaceResponseView.toString());
             searchFaceResponseViewList.add(searchFaceResponseView);
             //            处理QPS 超限。当前为2。
             Thread.sleep(500);
 
         }
-        File excel = new File("C:\\Users\\lastlySly\\Desktop\\test\\人脸搜索数据明细.xls");
-        searchFaceInfoToExcel(searchFaceResponseViewList,excel);
+//        File excel = new File("C:\\Users\\lastlySly\\Desktop\\test\\人脸搜索数据明细.xls");
+        searchFaceInfoToExcel(searchFaceResponseViewList,hssfWorkbook);
 
     }
 
@@ -251,12 +254,12 @@ public class MyUtils {
     /**
      * 人脸搜索数据导出到excel
      * @param searchFaceResponseViewList
-     * @param outFile
      * @throws IOException
      */
-    public static void searchFaceInfoToExcel(List<SearchFaceResponseView> searchFaceResponseViewList,File outFile) throws IOException {
+    public static void searchFaceInfoToExcel(List<SearchFaceResponseView> searchFaceResponseViewList,
+                                             HSSFWorkbook hssfWorkbook) throws IOException {
 
-        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+//        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
         HSSFSheet hssfSheet = hssfWorkbook.createSheet("人脸搜索数据明细");
         hssfSheet.setDefaultRowHeightInPoints(100);
         hssfSheet.setDefaultColumnWidth(30);
@@ -288,7 +291,7 @@ public class MyUtils {
         int row = 2;
         excelSetSearchFaceData(searchFaceResponseViewList, hssfWorkbook, hssfSheet, cellStyle, patriarch,row);
 
-        hssfWorkbook.write(outFile);
+//        hssfWorkbook.write(outFile);
     }
 
 
@@ -366,6 +369,236 @@ public class MyUtils {
     }
 
 
+
+
+
+
+
+
+    /**
+     * 扫描指定两个路径文件夹并对比人脸，最后存入excel
+     * @throws IOException
+     */
+    public static void scanFilesAndMatchFaceAndSaveToExcel(String filePath1,String filePath2,
+                                                           HSSFWorkbook hssfWorkbook) throws IOException, InterruptedException {
+
+        List<CustomMatchFaceDataView> customMatchFaceDataViewList = Lists.newArrayList();
+
+//        String filePath1 = "F:/MyAllWorkProject/xmgps/百度人脸识别评估/抓拍的车载照片/抓拍/";
+//        String filePath2 = "F:/MyAllWorkProject/xmgps/百度人脸识别评估/司机标准照片/新建文件夹 (2)/";
+        /**
+         * 文件路径集合1
+         */
+        List<String> filesPathList1 = Lists.newArrayList();
+        MyFileUtils.getFilesPathList(filePath1,filesPathList1);
+        List<Map<String,Object>> zpFileMapList = Lists.newArrayList();
+        /**
+         * 文件路径集合2
+         */
+        Map<String,List<File>> bzFileMap = Maps.newHashMap();
+        MyFileUtils.getFilesMap(filePath2,bzFileMap);
+
+        filesPathList1.forEach(fp ->{
+            File file = new File(fp);
+            Map<String,Object> map = Maps.newHashMap();
+            String carPlaceNum = file.getName().substring(0,7);
+            map.put("carPlaceNum",carPlaceNum);
+            map.put("file",file);
+            zpFileMapList.add(map);
+        });
+
+
+        //
+        for (int i = 0; i<zpFileMapList.size(); i++){
+            CustomMatchFaceDataView customMatchFaceDataView = new CustomMatchFaceDataView();
+            File zpFile = (File) zpFileMapList.get(i).get("file");
+            String zpFileName = (String) zpFileMapList.get(i).get("carPlaceNum");
+            customMatchFaceDataView.setCarPlaceNum(zpFileName);
+            customMatchFaceDataView.setZpFile(zpFile);
+
+            List<CustomBzFileAndScoreView> customBzFileAndScoreViewList = Lists.newArrayList();
+            if(bzFileMap.containsKey(zpFileName)){
+                List<File> bzFileList = bzFileMap.get(zpFileName);
+                for (File file : bzFileList){
+                    CustomBzFileAndScoreView customBzFileAndScoreView = new CustomBzFileAndScoreView();
+                    customBzFileAndScoreView.setBzFile(file);
+
+                    MatchFaceResponseView matchFaceResponseView = MyFaceRecognitionUtils.matchFace(zpFile,file);
+                    System.out.println("=======scanFilesAndMatchFaceAndSaveToExcel========");
+                    System.out.println(matchFaceResponseView);
+                    customBzFileAndScoreView.setError_code(matchFaceResponseView.getError_code());
+                    customBzFileAndScoreView.setError_msg(matchFaceResponseView.getError_msg());
+                    if (matchFaceResponseView.getError_code() == 0){
+                        customBzFileAndScoreView.setScore(matchFaceResponseView.getResult().getScore());
+                    }
+
+                    customBzFileAndScoreViewList.add(customBzFileAndScoreView);
+                }
+            }
+            customMatchFaceDataView.setCustomBzFileAndScoreViewList(customBzFileAndScoreViewList);
+            customMatchFaceDataViewList.add(customMatchFaceDataView);
+
+            //            处理QPS 超限。当前为2。
+            Thread.sleep(500);
+
+        }
+
+
+
+        matchFaceInfoToExcel(customMatchFaceDataViewList,hssfWorkbook);
+
+    }
+
+
+
+
+
+
+    /**
+     * 人脸对比数据导出到excel
+     * @param customMatchFaceDataViewList
+     * @throws IOException
+     */
+    public static void matchFaceInfoToExcel(List<CustomMatchFaceDataView> customMatchFaceDataViewList,HSSFWorkbook hssfWorkbook) throws IOException {
+
+//        HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+        HSSFSheet hssfSheet = hssfWorkbook.createSheet("人脸对比数据");
+        hssfSheet.setDefaultRowHeightInPoints(100);
+        hssfSheet.setDefaultColumnWidth(30);
+        HSSFCellStyle cellStyle=hssfWorkbook.createCellStyle();
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        cellStyle.setWrapText(true);
+        //画图的顶级管理器，一个sheet只能获取一个（一定要注意这点）
+        HSSFPatriarch patriarch = hssfSheet.createDrawingPatriarch();
+
+        HSSFRow hssfRow1 = hssfSheet.createRow(0);
+        HSSFCell cell = hssfRow1.createCell(0);
+        cell.setCellStyle(cellStyle);
+        cell.setCellValue("人脸对比数据明细");
+        //合并单元格CellRangeAddress构造参数依次表示起始行，截至行，起始列， 截至列
+        hssfSheet.addMergedRegion(new CellRangeAddress(0,0,0,7));
+        //在sheet里创建第二行
+        HSSFRow row2=hssfSheet.createRow(1);
+        //创建单元格并设置单元格内容
+        row2.createCell(0).setCellValue("抓拍图片");
+        row2.createCell(1).setCellValue("车牌号");
+        row2.createCell(2).setCellValue("人脸相似度");
+        row2.createCell(3).setCellValue("标准人脸图片");
+        Iterator<Cell> iterator = row2.cellIterator();
+        while (iterator.hasNext()){
+            iterator.next().setCellStyle(cellStyle);
+        }
+        int row = 2;
+
+        excelSetMatchFaceData(customMatchFaceDataViewList, hssfWorkbook, hssfSheet, cellStyle, patriarch,row);
+//        hssfWorkbook.write(outFile);
+    }
+
+    /**
+     * 人脸对比excel数据填充
+     * @param customMatchFaceDataViewList
+     * @param hssfWorkbook
+     * @param hssfSheet
+     * @param cellStyle
+     * @param patriarch
+     * @param row
+     * @throws IOException
+     */
+    private static void excelSetMatchFaceData(List<CustomMatchFaceDataView> customMatchFaceDataViewList,
+                                              HSSFWorkbook hssfWorkbook, HSSFSheet hssfSheet,
+                                              HSSFCellStyle cellStyle, HSSFPatriarch patriarch,
+                                              int row) throws IOException {
+        Iterator<Cell> iterator;
+        for (int i = 0; i < customMatchFaceDataViewList.size(); i++){
+            CustomMatchFaceDataView customMatchFaceDataView = customMatchFaceDataViewList.get(i);
+            File zpFile = customMatchFaceDataView.getZpFile();
+            ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();
+            BufferedImage bufferImg = ImageIO.read(zpFile);
+            ImageIO.write(bufferImg, "jpg", byteArrayOut);
+
+            int matchFileSize = customMatchFaceDataView.getCustomBzFileAndScoreViewList().size();
+
+//            找不到匹配车牌号的标准人脸照片
+            if (matchFileSize == 0){
+                hssfSheet.addMergedRegion(new CellRangeAddress(row,row,2,3));
+                HSSFRow rowError=hssfSheet.createRow(row);
+                HSSFCell errorCell1 = rowError.createCell(1);
+                errorCell1.setCellValue(customMatchFaceDataView.getCarPlaceNum());
+                errorCell1.setCellStyle(cellStyle);
+
+                HSSFCell errorCell2 = rowError.createCell(2);
+                errorCell2.setCellValue("找不到匹配车牌号的标准人脸照片");
+                errorCell2.setCellStyle(cellStyle);
+
+                //抓拍图片插入，，file1
+                HSSFClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 255,(short) 0, row, (short) 1, row);
+                anchor.setAnchorType(ClientAnchor.AnchorType.DONT_MOVE_AND_RESIZE);
+                //插入图片
+                patriarch.createPicture(anchor, hssfWorkbook.addPicture(byteArrayOut.toByteArray(), HSSFWorkbook.PICTURE_TYPE_JPEG));
+
+                byteArrayOut.close();
+
+                row ++;
+                continue;
+            }
+
+            if (matchFileSize > 1){
+                //合并单元格CellRangeAddress构造参数依次表示起始行，截至行，起始列， 截至列
+                hssfSheet.addMergedRegion(new CellRangeAddress(row,row+matchFileSize-1,0,0));
+                hssfSheet.addMergedRegion(new CellRangeAddress(row,row+matchFileSize-1,1,1));
+            }
+
+            //抓拍图片插入，，file1
+            HSSFClientAnchor anchor = new HSSFClientAnchor(0, 0, 0, 255,(short) 0, row, (short) 1, row+matchFileSize-1);
+            anchor.setAnchorType(ClientAnchor.AnchorType.DONT_MOVE_AND_RESIZE);
+            //插入图片
+            patriarch.createPicture(anchor, hssfWorkbook.addPicture(byteArrayOut.toByteArray(), HSSFWorkbook.PICTURE_TYPE_JPEG));
+
+            byteArrayOut.close();
+
+            /**
+             * 遍历比对完的数据
+             */
+            for (int j = 0; j<matchFileSize; j++){
+
+                /**
+                 * 获取比对完的信息
+                 */
+                CustomBzFileAndScoreView customBzFileAndScoreView = customMatchFaceDataView
+                        .getCustomBzFileAndScoreViewList().get(j);
+
+                ByteArrayOutputStream bzByteArrayOut = new ByteArrayOutputStream();
+                BufferedImage bzBufferImg = ImageIO.read(customBzFileAndScoreView.getBzFile());
+                ImageIO.write(bzBufferImg, "jpg", bzByteArrayOut);
+
+                //标准图片插入，，file2
+                HSSFClientAnchor bzAnchor = new HSSFClientAnchor(0, 0, 0, 255,(short) 3, row, (short) 4, row);
+                bzAnchor.setAnchorType(ClientAnchor.AnchorType.DONT_MOVE_AND_RESIZE);
+                //插入图片
+                patriarch.createPicture(bzAnchor, hssfWorkbook.addPicture(bzByteArrayOut.toByteArray(), HSSFWorkbook.PICTURE_TYPE_JPEG));
+
+                HSSFRow row3=hssfSheet.createRow(row);
+                row3.createCell(1).setCellValue(customMatchFaceDataView.getCarPlaceNum());
+
+                if (customBzFileAndScoreView.getError_code() == 0){
+                    String score = customBzFileAndScoreView.getScore() == null ? "" : customBzFileAndScoreView.getScore().toString();
+                    row3.createCell(2).setCellValue(score);
+                }else{
+                    row3.createCell(2).setCellValue(customBzFileAndScoreView.getError_msg());
+                }
+
+
+                iterator = row3.cellIterator();
+                while (iterator.hasNext()){
+                    iterator.next().setCellStyle(cellStyle);
+                }
+                row ++;
+
+            }
+
+        }
+    }
 
 
 
